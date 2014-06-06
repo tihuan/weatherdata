d02949e64a4d903773e8d8cd7cccf12a760dba43
Timmy Huang <timmyhuang0404@gmail.com>
2014-06-05 23:50:11 -0400

    feature working

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,5 +1,13 @@
 class CitiesController < ApplicationController
   def index
     @cities = City.all
+    @weathers = City.first.weathers
+    @city_temps = city_temps
+  end
+
+  def city_temps
+    weathers = City.first.weathers
+    weather_temp_pairs = weathers.map { |weather| [weather.created_at.to_s, weather.temp] }
+    p hourly_temps = Hash[weather_temp_pairs]
   end
 end
