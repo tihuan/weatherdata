8cdf6652dbf1e0f249b0639070847debaf50d42c
Timmy Huang <timmyhuang0404@gmail.com>
2014-06-08 17:37:06 -0400

    move #city_temps to private method

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,14 +1,19 @@
 class CitiesController < ApplicationController
   def index
     @cities = City.all
-    @weathers_first12 = City.first.weathers.last(24)[0..11]
-    @weathers_last12 = City.first.weathers.last(24)[12..23]
+  end
+
+  def show
+    @city = City.find(params[:id])
+    @weathers_first12 = @city.weathers.last(24)[0..11]
+    @weathers_last12 = @city.weathers.last(24)[12..23]
     @city_temps = city_temps
   end
 
-  def city_temps
-    weathers = City.first.weathers.last(24)
-    weather_temp_pairs = weathers.map { |weather| [weather.localtime.to_s, weather.temp] }
-    p hourly_temps = Hash[weather_temp_pairs]
-  end
+  private
+    def city_temps
+      weathers = City.find(params[:id]).weathers.last(24)
+      weather_temp_pairs = weathers.map { |weather| [weather.localtime.to_s, weather.temp] }
+      hourly_temps = Hash[weather_temp_pairs]
+    end
 end
