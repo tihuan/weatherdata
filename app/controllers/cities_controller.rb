6a9d43bea241eccce532cad86bc7dfd8592fd9d6
Timmy Huang <timmyhuang0404@gmail.com>
2014-06-10 23:07:21 -0400

    add before_filter to auto load @cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,4 +1,6 @@
 class CitiesController < ApplicationController
+  before_filter :load_cities
+
   def index
     @cities = City.all
   end
@@ -10,10 +12,14 @@ class CitiesController < ApplicationController
     @city_temps = city_temps
   end
 
-  private
-    def city_temps
-      weathers = City.find(params[:id]).weathers.last(24)
-      weather_temp_pairs = weathers.map { |weather| [weather.localtime.to_s, weather.temp] }
-      hourly_temps = Hash[weather_temp_pairs]
-    end
+private
+  def load_cities
+    @cities = City.all
+  end
+
+  def city_temps
+    weathers = City.find(params[:id]).weathers.last(24)
+    weather_temp_pairs = weathers.map { |weather| [weather.localtime.to_s, weather.temp] }
+    hourly_temps = Hash[weather_temp_pairs]
+  end
 end
