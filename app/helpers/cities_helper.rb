17658427bf9cc7bbcde7cb605b2cec0c653b234c
Timmy Huang <timmyhuang0404@gmail.com>
2014-06-13 03:23:18 -0400

    fix bug in helper and show.html.erb

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,7 +1,7 @@
 module CitiesHelper
   def arrow(city_id, direction)
-    if City.exists?(@city.id - 1)
-      link_to('', city_path(id: @city.id - 1), class: "fa fa-chevron-#{direction} fa-4x arrow-#{direction}", remote: true)
+    if City.exists?(city_id)
+      link_to('', city_path(id: city_id), class: "fa fa-chevron-#{direction} fa-4x arrow-#{direction}", remote: true)
     end
   end
 end
