1917c1a0796e05a408ea762a78fead2c4f0b433c
Timmy Huang <timmyhuang0404@gmail.com>
2014-06-13 03:09:46 -0400

    create helper method to refactor arrows

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -1,2 +1,7 @@
 module CitiesHelper
+  def arrow(city_id, direction)
+    if City.exists?(@city.id - 1)
+      link_to('', city_path(id: @city.id - 1), class: "fa fa-chevron-#{direction} fa-4x arrow-#{direction}", remote: true)
+    end
+  end
 end
