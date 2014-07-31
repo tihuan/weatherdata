b9d805e084a8a42e32956d2273d2f147d75bb2ae
Timmy Huang <timmyhuang0404@gmail.com>
2014-07-30 20:05:12 -0700

    Refactor: extract #display_table to helper

--- app/helpers/cities_helper.rb
+++ app/helpers/cities_helper.rb
@@ -4,4 +4,12 @@ module CitiesHelper
       link_to('', city_path(id: city_id), class: "fa fa-chevron-#{direction} fa-4x arrow-#{direction}", remote: true)
     end
   end
+
+  def display_table(weathers)
+    raw(
+      weathers.each_slice(6).map do |weather_row|
+        render 'table', weathers: weather_row
+      end.join
+    )
+  end
 end
